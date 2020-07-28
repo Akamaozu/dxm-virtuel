d7400d9295fa9f5f3fdaaaf67ab8316d0daa2c97
Uzo Olisemeka <uzo@designbymobius.ca>
2020-07-28 13:05:06 -0400

    load env variables before starting server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var supervisor = require('supe')(),
     path = require('path'),
+    dotenv = require('dotenv'),
     path_to_root = path.join( __dirname );
+
+dotenv.config();
 
 supervisor.hook.add( 'server-excessive-crash', 'exit', function( details ){
   exit_app( new Error( 'server crashed excessively ('+ details.max_retries +' times in '+ details.duration +' mins)' ) );
