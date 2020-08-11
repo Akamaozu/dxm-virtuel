799621befee2dc62f3be31413c5af26252348cd1
Uzo Olisemeka <uzo@designbymobius.ca>
2020-08-11 16:10:39 -0400

    responsive video embed + minor style cleanups

--- citizens/server/js/behavior.js
+++ citizens/server/js/behavior.js
@@ -1,6 +1,71 @@
 var socketio = require('socket.io-client'),
     create_task = require('cjs-task'),
+    debounce = require('debounce'),
     app = create_task();
+
+app.step( 'wait for document to load', function(){
+  if( document.readyState == 'interactive' ) return on_document_ready();
+  else document.addEventListener( 'DOMContentLoaded', on_document_ready );
+
+  function on_document_ready(){
+    console.log( 'action=log-document-ready' );
+    app.next();
+  }
+});
+
+app.step( 'get / autoupdate window size', function(){
+  window.addEventListener( 'resize', debounce( get_window_size, 99 , false ) );
+  get_window_size();
+
+  app.next();
+
+  function get_window_size(){
+    var window_dimensions = {};
+        window_dimensions.width = window.innerWidth;
+        window_dimensions.height = window.innerHeight;
+
+    app.set( 'window-size', window_dimensions );
+    console.log( 'action=get-window-size width='+ window_dimensions.width +'px height='+ window_dimensions.height +'px' );
+  }
+});
+
+app.step( 'make video embed responsive', function(){
+  var video_embed = document.querySelector( '#video-embed' ),
+      window_size = app.get( 'window-size' ),
+      video_parent = video_embed.parentNode,
+      initial_dimensions = get_responsive_embed_stats();
+
+  var video_embed_aspect_ratio = video_embed.offsetWidth / video_embed.offsetHeight;
+
+  console.log( 'action=log-initial-embed-dimensions width='+ video_embed.offsetWidth +'px height='+ video_embed.offsetHeight +'px ratio='+ video_embed_aspect_ratio );
+
+  update_responsive_embed({ w: initial_dimensions.video_parent.w, h: initial_dimensions.video_parent.w / video_embed_aspect_ratio });
+
+  app.hook.add( 'value-updated', 'update-embed-dimensions', function( details ){
+    if( details.key !== 'window-size' ) return;
+
+    var stats = get_responsive_embed_stats();
+    update_responsive_embed({ w: stats.video_parent.w, h: stats.video_parent.w / video_embed_aspect_ratio });
+  });
+
+  app.next();
+
+  function update_responsive_embed( dimensions ){
+    video_embed.style.width = dimensions.w +'px';
+    video_embed.style.height = dimensions.h +'px';
+
+    console.log( 'action=update-embed-dimensions width='+ dimensions.w +'px height='+ dimensions.h +'px' );
+  }
+
+  function get_responsive_embed_stats(){
+    var stats = {};
+
+    stats.window = { w: window_size.width, h: window_size.height };
+    stats.video_parent = { w: video_parent.offsetWidth, h: video_parent.offsetHeight };
+
+    return stats;
+  }
+});
 
 app.step( 'setup socket.io', function(){
   var socketio_client = socketio();
